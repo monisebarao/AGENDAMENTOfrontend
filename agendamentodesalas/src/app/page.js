0fb80cc19a44232f2a6a65536d99483864e9205b
monise <monisecbarao@gmail.com>
2024-09-23 09:41:55 -0300

    footer e header

--- agendamentodesalas/src/app/page.js
+++ agendamentodesalas/src/app/page.js
@@ -1,10 +1,11 @@
 import Header from "@/components/header";
 import Card from "@/components/cards";
+import Footer from "@/components/footer";
 
 export default function Corpo() {
   return (
     <div className={style.fundo}>
-      <Header></Header>
+      <Header/>
       <Card
         image="https://example.com/image.jpg"
         alt="An example image"
@@ -12,6 +13,7 @@ export default function Corpo() {
         description="This is an example card component"
         onClick={() => console.log('Card clicked!')}
       />
+      <Footer/>
     </div>
   );
 }
