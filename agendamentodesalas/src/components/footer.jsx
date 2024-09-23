3c1b0385eeffe5269d1667df55d5859903d61525
Biancavshutz <bianca.vshutz@gmail.com>
2024-09-23 13:35:41 -0300

    footer arrumado

--- agendamentodesalas/src/components/footer.jsx
+++ agendamentodesalas/src/components/footer.jsx
@@ -3,25 +3,24 @@ import styles from '@/app/page.module.css'
 export default function Footer() {
   return (
     <div className={styles.footerhome}>
-      <div className={styles.footer2}>
-        <div className={styles.thome}>
+        <div className={styles.escola}>
           <h1>Escola SESI-SP de Vinhedo (CE 242)</h1>
           <br />
-          <h1 className={styles.thm1}>
+          <h1 className={styles.endereco}>
             Av. Ana Lombardi Gasparini, 1155 - Jardim Nova Canudos
           </h1>
         </div>
         <br />
-        <div className={styles.thome2}>
+        <div className={styles.curso}>
           <h1>Desenvolvimento de Sistemas - SENAI</h1>
           <br />
-          <h1 className={styles.thm}>Grupo 6 - Curso Técnico</h1>
+          <h1 className={styles.grupo}>Grupo 6 - Curso Técnico</h1>
         </div>
         <br />
-        <div className={styles.thome3}>
+       
+        <div className={styles.copyright}>
           <h1>Copyright 2024 © Todos os direitos reservados. - DM0</h1>
         </div>
       </div>
-    </div>
   );
 }
