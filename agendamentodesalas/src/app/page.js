bb3685ae11472dbc7933e81bb5282712b611e9bf
Isabela da Silva Biacca <isa.biacca08@gmail.com>
2024-09-24 11:27:31 -0300

    subindo barra de navegação e o titulo de introdução

--- agendamentodesalas/src/app/page.js
+++ agendamentodesalas/src/app/page.js
@@ -12,8 +12,28 @@ import senai from "../public/salasenai.jpeg";
 export default function Corpo() {
   return (
     <div>
+      {/* HEADER */}
       <Header />
 
+      {/* NAVEGAÇÃO */}
+      <section className="disposicaoNav">
+
+        <div className="salaNav"><a href="">SALAS</a></div>
+
+        <div className="equipNav"><a href="">EQUIPAMENTOS</a></div>
+
+      </section>
+      
+      <br/>
+      <br/>
+      <br/>
+
+      {/* TITULO PAGINA */}
+      <section className="tituloGeral">
+        <h3>Faça seu agendamento aqui</h3>
+      </section>
+
+      {/* SEÇÃO CARDS */}
       <section className="disposicaoCards">
         <Card
           image={lmt}
