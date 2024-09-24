ee54295a91e8024548aafa802076b429155c194d
Isabela da Silva Biacca <isa.biacca08@gmail.com>
2024-09-24 09:51:25 -0300

    responsivo dos cards agendamento salas

--- agendamentodesalas/src/components/cards.jsx
+++ agendamentodesalas/src/components/cards.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Card (props) {
     return(
-        <div className='unidadecard'>
+        <div className='unidadeCard'>
             <Image src={props.image} alt={props.alt} width={props.width} height={props.height} className='cardImg'/>
             <h2 className='tituloCard'>{props.title}</h2>
             <button onClick={props.onClick} className='cardButton'>Agendar</button>
