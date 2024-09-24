d36298d166066fa822d1d305ef565a03bc2fcb4d
monise <monisecbarao@gmail.com>
2024-09-24 08:31:07 -0300

    header jsx

--- agendamentodesalas/src/components/header.jsx
+++ agendamentodesalas/src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       
       <div>
         <hr className={styles.linha} />
-        <Image src={Logo} width="100" height={60} />
+        <Image className={styles.logo} src={Logo} width="100" height={60} />
       </div>
       <div className={styles.headerconteiner}>
 
